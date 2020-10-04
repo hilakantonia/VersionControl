afd45561c91710497675da12ca7328a6f7aaf295
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-10-04 19:16:42 +0200

    utolsó

--- Excelgen/Excelgen/Form1.cs
+++ Excelgen/Excelgen/Form1.cs
@@ -99,6 +99,18 @@ namespace Excelgen
             GetCell(2, 1),
             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
 
+            //9.oszlop
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+
 
         }
         private string GetCell(int x, int y)
