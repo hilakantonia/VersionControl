d717b2f347c50b7e69aaf0a636a6a21bae10552f
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-10-25 16:52:42 +0100

    osztályok

---
+++ mikroszimulacio/mikroszimulacio/Entities/Birthprobability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszimulacio.Entities
+{
+    public class Birthprobability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+    }
+}
