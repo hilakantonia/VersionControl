39880ddbbaa4fa7373f9334f498d121609b20335
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-11-16 10:13:47 +0100

    termékválasztó gombok

--- factorypattern/factorypattern/Form1.Designer.cs
+++ factorypattern/factorypattern/Form1.Designer.cs
@@ -32,13 +32,16 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnSelectCar = new System.Windows.Forms.Button();
+            this.btnSelectBall = new System.Windows.Forms.Button();
+            this.lblNext = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainPanel.Location = new System.Drawing.Point(12, 44);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(776, 426);
+            this.mainPanel.Size = new System.Drawing.Size(776, 394);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -49,15 +52,46 @@
             // 
             this.conveyorTimer.Interval = 10;
             // 
+            // btnSelectCar
+            // 
+            this.btnSelectCar.Location = new System.Drawing.Point(13, 13);
+            this.btnSelectCar.Name = "btnSelectCar";
+            this.btnSelectCar.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectCar.TabIndex = 1;
+            this.btnSelectCar.Text = "CAR";
+            this.btnSelectCar.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectBall
+            // 
+            this.btnSelectBall.Location = new System.Drawing.Point(95, 13);
+            this.btnSelectBall.Name = "btnSelectBall";
+            this.btnSelectBall.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectBall.TabIndex = 2;
+            this.btnSelectBall.Text = "BALL";
+            this.btnSelectBall.UseVisualStyleBackColor = true;
+            // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(422, 13);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(89, 17);
+            this.lblNext.TabIndex = 3;
+            this.lblNext.Text = "Coming next:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.btnSelectBall);
+            this.Controls.Add(this.btnSelectCar);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +100,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button btnSelectCar;
+        private System.Windows.Forms.Button btnSelectBall;
+        private System.Windows.Forms.Label lblNext;
     }
 }
 
