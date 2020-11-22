33537baf42ef0083a1a3874343ff747817f0adb9
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-11-22 20:40:08 +0100

    jelszó validálás teszt

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -28,5 +28,28 @@ namespace UnitTestExample.Test
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+
+        [
+         Test,
+         TestCase("ABCD1234", false),
+         TestCase("Ab1234", false),
+         TestCase("Abcd1234", true),
+         TestCase("abcd1234", false),
+        TestCase("abcdABCD", false)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
     }
 }
