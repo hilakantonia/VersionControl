8e9e9a49d2b6e41c7ff8371c6a85d2ec94dabb23
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-11-29 21:01:42 +0100

    kipróbálás

--- Evolucios_algoritmus/Evolucios_algoritmus/Form1.cs
+++ Evolucios_algoritmus/Evolucios_algoritmus/Form1.cs
@@ -22,6 +22,9 @@ namespace Evolucios_algoritmus
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
