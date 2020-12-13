e30d0083dcc5c4a3ebf343a56a8e8c2c3a3bc68e
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-12-13 22:47:12 +0100

    Bindinglist

---
+++ Webszolgáltatás/Webszolgáltatás/Entities/RateData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webszolgáltatás.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public String Currency { get; set; }
+        public Decimal Value { get; set; }
+
+
+    }
+}
--- Webszolgáltatás/Webszolgáltatás/Form1.cs
+++ Webszolgáltatás/Webszolgáltatás/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgáltatás.Entities;
+using Webszolgáltatás.MnbServiceReference;
 
 namespace Webszolgáltatás
 {
@@ -15,6 +17,25 @@ namespace Webszolgáltatás
         public Form1()
         {
             InitializeComponent();
+            Webszolg();
+
+            BindingList<RateData> Rates = new BindingList<RateData>();
+            dataGridView1.DataSource = Rates;
+
+
+        }
+
+        private void Webszolg()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
