1cecf2b3847987dc17cd1897a45b918c7a89b175
Antónia Hilák <antonia.hilak@stud.uni-corvinus.hu>
2020-12-13 23:08:48 +0100

    Chart

--- Webszolgáltatás/Webszolgáltatás/Form1.cs
+++ Webszolgáltatás/Webszolgáltatás/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Webszolgáltatás.Entities;
 using Webszolgáltatás.MnbServiceReference;
@@ -24,8 +25,6 @@ namespace Webszolgáltatás
             dataGridView1.DataSource = Rates;
 
             XMLfeldolg();
-
-
         }
 
 
@@ -62,6 +61,22 @@ namespace Webszolgáltatás
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
+
+                chartRateData.DataSource = Rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
             }
         }
     }
